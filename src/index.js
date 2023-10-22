df5042e03f0fc8d9234c14ff8a2c3a11c643038f
Julian Fragoso <starsmember@yahoo.com>
2023-10-22 18:29:06 -0500

    Added more robust configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+import './helpers/dotenv'
+
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import logger from './helpers/logger'
+import router from './routers'
+import { notFound, errorHandler } from './helper/errors'
+
+const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
-const port = process.env.PORT || 3000
-
-app.get('/', (req,res) => {
-    res.send({msg: "Hello There"})
-})
 
 
-app.listen(port)
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helmet())
+
+app.use(router)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () =>
+logger.info(`Application started at http://localhost:${process.env.PORT}`),
+)
