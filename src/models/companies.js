ca2dc699ba76b77c84df9e48d0a4d11a2f56de18
Julian Fragoso <starsmember@yahoo.com>
2023-11-06 17:28:21 -0600

    added department creation

--- src/models/companies.js
+++ src/models/companies.js
@@ -2,6 +2,12 @@ import db from '../helpers/db'
 
 export const getCompanies = async () => db.company.findMany()
 
-export const getCompany = async (id) => db.company.findUnique({ where: { companyId } })
+export const getCompany = async (id) =>
+    db.company.findUnique({
+        where: { companyId: id },
+        include: {
+            department: true,
+        },
+    })
 
 export const addCompany = async (name) => db.company.create({ data: { name } })
