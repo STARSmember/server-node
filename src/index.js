c04bdefde27f3681615cb6e74b44e79f299c88f2
Julian Fragoso <starsmember@yahoo.com>
2023-11-27 10:42:38 -0600

    API restart

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './helpers/dotenv'
+import './helpers/config'
 
 import express from 'express'
 import morgan from 'morgan'
@@ -6,25 +6,30 @@ import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
 
+
 import logger from './helpers/logger'
 import router from './routes'
 import { notFound, errorHandler } from './helpers/errors'
 
-const port = process.env.PORT || 3000
+const port = Number(process.env.PORT)
 
 const app = express()
 
-
 app.use(morgan(process.env.MORGAN_LOG))
-app.use(cors({ origin: process.env.ORIGIN }))
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        exposedHeaders: ['x-total-count', 'x-total-page'],
+    }),
+)
 app.use(helmet())
 app.use(bodyParser.json())
 
-app.use(router)
+app.use('/', router)
 
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () =>
-logger.info(`Application started at http://localhost:${process.env.PORT}`),
-)
+app.listen(port, () => {
+    logger.info(`Server running on port ${port}`)
+})
